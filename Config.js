a054079672c48307c75c9ce02c12611a9858ed0e
stroonsN <84482323+stroonsN@users.noreply.github.com>
2021-05-26 09:44:24 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,4 +1,4 @@
-module.exports.www_path = "www";
+module.exports.www_path = "public_html";
 module.exports.default_port = 80;
 module.exports.php_path = "/usr/bin/php";
 module.exports.index_file = "index.php";
